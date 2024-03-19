501f8aa4500f35d9b657a41220d6ab2fc3d0f57a
Dorian Poleni <dpoleni@yahoo.it>
2024-03-19 09:51:56 +0100

    first commit

---
+++ esercizio1.js
@@ -0,0 +1,47 @@
+//Tag Declaration
+let buttonHide = document.querySelector("#buttonHide");
+let buttonChangeColors = document.querySelector("#buttonChangeColors");
+let buttonMakeBold = document.querySelector("#buttonMakeBold");
+let p = document.querySelectorAll("p");
+
+//Main
+
+//ButtonHide
+buttonHide.addEventListener("click", ()=> {
+    p.forEach( p => {
+        p.classList.toggle("d-none");
+    })
+})
+
+//ButtonChangeColor
+
+buttonChangeColors.addEventListener("click", () => {
+    p.forEach( (p) => {
+        p.style.color = getRandomColorHex();
+        // p.style.color = `rgb(${r},${g},${b})`; 
+    })
+});
+
+//buttonMakeBold
+buttonMakeBold.addEventListener("click", () => {
+    p.forEach( (p) => {
+        // p.style.fontWeight = "bold"; 
+        p.classList.toggle("fw-bold");
+    })
+})
+
+
+//Functions declaration
+
+function getRandomColorHex() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function setRandomColor() {
+    console.log(getRandomColorHex());
+}
---
+++ esercizio2.js
@@ -0,0 +1,31 @@
+//Tag
+let people = [
+    {name: "Dorian", surname:"Poleni", age: "34", place: "Bergamo", img: "https://picsum.photos/200"},
+    {name: "Dorian", surname:"Poleni", age: "34", place: "Bergamo", img: "https://picsum.photos/200"},
+];
+
+
+people.forEach(person => {
+    
+    let personCard = document.createElement("div");
+    
+    personCard.classList.add("col-4");
+    
+    personCard.innerHTML = `<div class="card">
+                                <img src="${person.img}" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                    <h5 class="card-title" id="personName">${person.name}</h5>
+                                    <p class="card-text" >${person.place}</p>
+                                    <a href="#" class="btn btn-primary">info</a>
+                                </div>
+                            </div>`
+    
+    let cardContainer = document.querySelector("#cardContainer");
+    
+    cardContainer.appendChild(personCard);
+    
+    console.log(personCard)
+});
+
+
+
